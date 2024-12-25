3fe3eb2b0213df6a19df22da8249de751ca175db
Bengoro <127534872+Bengoro1@users.noreply.github.com>
2024-12-25 19:18:59 +0100

    add filter bar

--- controllers/controller.js
+++ controllers/controller.js
@@ -9,17 +9,27 @@ async function allCategoriesGet(req, res) {
 }
 
 async function componentGet(req, res) {
-  const component = await db.getComponent(req.params.pc_component);
+  const component = Object.keys(req.query).length > 0 ? await db.getFilteredItems(req.params.pc_component, req.query) : await db.getComponent(req.params.pc_component);
   const title = await db.getCategoryName(req.params.pc_component);
+  const filterBarColumns = await db.getFilterBarColumns(req.params.pc_component);
+  const filterBar = await Promise.all(
+    filterBarColumns
+      .filter(column => column.column_name != 'id')
+      .map(column => db.getFilterBarRows(req.params.pc_component, column.column_name))
+  );
+  console.log(req.params.pc_component);
   res.render('category', {
-    title: title[0].name,
-    component: component
+    title: title.name,
+    component: component,
+    filterBar: filterBar,
+    component_url: req.params.pc_component
   });
 }
 
 async function productGet(req, res) {
   const product = await db.getProduct(req.params.pc_component, req.params.product);
   res.render('product', {
+    title: product.name,
     product: product
   });
 }
@@ -27,5 +37,5 @@ async function productGet(req, res) {
 module.exports = {
   allCategoriesGet,
   componentGet,
-  productGet
+  productGet,
 }
--- db/queries.js
+++ db/queries.js
@@ -11,18 +11,40 @@ async function getComponent(component) {
 }
 
 async function getCategoryName(category) {
-  const { rows } = await pool.query(`SELECT name FROM pc_components WHERE url = $1`, [category]);
-  return rows;
+  const { rows } = await pool.query(`SELECT name FROM pc_components WHERE url = $1;`, [category]);
+  return rows[0];
 }
 
 async function getProduct(category, product) {
-  const { rows } = await pool.query(`SELECT * FROM ${category} WHERE id = $1`, [product]);
+  const { rows } = await pool.query(`SELECT * FROM ${category} WHERE id = $1;`, [product]);
+  return rows[0];
+}
+
+async function getFilterBarColumns(component) {
+  const { rows } = await pool.query(`SELECT column_name
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = N'${component}';
+  `);
   return rows;
+}
+
+async function getFilterBarRows(component, column) {
+  const result = await pool.query(`SELECT DISTINCT ${column} FROM ${component};`);
+  const values = result.rows.map(value => value[result.fields[0].name]);
+  const row = {[result.fields[0].name]: values};
+  return row;
+}
+
+async function getFilteredItems(component, query) {
+  console.log(component, query);
 }
 
 module.exports = {
   getAllCategories,
   getComponent,
   getCategoryName,
-  getProduct
+  getProduct,
+  getFilterBarColumns,
+  getFilterBarRows,
+  getFilteredItems
 }
